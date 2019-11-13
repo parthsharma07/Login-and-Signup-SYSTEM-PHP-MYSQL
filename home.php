102b66fedd58da0b4a98b8d22b6d4690b34fd23d
PARTH <parthcricketer07@gmail.com>
2019-11-13 10:07:37 +0530

    train availability page almost complete

--- home.php
+++ home.php
@@ -2,10 +2,10 @@
 // We need to use sessions, so you should always start sessions using the below code.
 session_start();
 // If the user is not logged in redirect to the login page...
-if (!isset($_SESSION['loggedin'])) {
-	header('Location: loginpage.html');
-	exit();
-}
+// if (!isset($_SESSION['loggedin'])) {
+// 	header('Location: loginpage.html');
+// 	exit();
+// }
 ?>
 
 <!DOCTYPE html>
