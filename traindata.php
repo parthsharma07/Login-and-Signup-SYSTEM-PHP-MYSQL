874307083759dea09dc49475d09ba724c379c106
PARTH <parthcricketer07@gmail.com>
2019-11-17 15:35:28 +0530

    traindata added

--- traindata.php
+++ traindata.php
@@ -1,15 +1,15 @@
 <?php
 // Array with names
-$a[] = "Maharashtra-Goa~Goa Express~190123~Mumbai~Panji~Mon,Thurs~23:00~11:00";
-$a[] = "Madhya Pradesh-Goa~South Express~190823~Indore~Panji~Mon,Thurs~23:00~11:00";
-$a[] = "Madhya Pradesh-Goa~Central Express~190123~Bhopal~Panji~Tues,Fri~22:00~9:00";
-$a[] = "Punjab-Haryana~North Express~170123~Jalandhar~Kurukshetra~Tues,Thurs,Sat~23:00~11:00";
-$a[] = "Madhya Pradesh-Goa~Malwa Express~190129~Gwalior~Panji~Tues,Fri~22:00~9:00";
-$a[] = "Maharashtra-Goa~Goatawi Express~110123~Nagpur~Panji~Mon,Thurs~23:00~11:00";
-$a[] = "Bihar-Madhya Pradesh~BMP Express~160103~Patna~Indore~Mon,Fri~23:55~23:00";
-$a[] = "Bihar-Madhya Pradesh~Kranti Express~150103~Patna~Indore~Mon,Sat~23:55~23:00";
-$a[] = "Maharashtra-Goa~Atal Express~190100~Mumbai~Panji~Mon,Fri~20:00~1:00";
-$a[] = "Punjab-Madhya Pradesh~Nirantar Express~190999~Chandigarh~Indore~Mon,Thurs~13:00~11:00";
+$a[] = "Mumbai-Panji~Goa Express~190123~Mumbai~Panji~Mon,Thurs~23:00~11:00";
+$a[] = "Indore-Panji~South Express~190823~Indore~Panji~Mon,Thurs~23:00~11:00";
+$a[] = "Bhopal-Panji~Central Express~190923~Bhopal~Panji~Tues,Fri~22:00~9:00";
+$a[] = "Jalandhar-Kurukshetra~North Express~170123~Jalandhar~Kurukshetra~Tues,Thurs,Sat~23:00~11:00";
+$a[] = "Gwalior-Panji~Malwa Express~190129~Gwalior~Panji~Tues,Fri~22:00~9:00";
+$a[] = "Nagpur-Panji~Goatawi Express~110123~Nagpur~Panji~Mon,Thurs~23:00~11:00";
+$a[] = "Patna-Indore~BMP Express~160103~Patna~Indore~Mon,Fri~23:55~23:00";
+$a[] = "Patna-Indore~Kranti Express~150103~Patna~Indore~Mon,Sat~23:55~23:00";
+$a[] = "Mumbai-Panji~Atal Express~190100~Mumbai~Panji~Mon,Fri~20:00~1:00";
+$a[] = "Chandigarh-Indore~Nirantar Express~190999~Chandigarh~Indore~Mon,Thurs~13:00~11:00";
 
 // get the q parameter from URL
 $q = $_REQUEST["q"];
